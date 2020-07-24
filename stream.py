42fcdcaab5ac5816638dea022c9c8873a2ce83a6
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-07-24 16:00:10 +0530

    using rgb and hsv colour combination

--- stream.py
+++ stream.py
@@ -10,3 +10,9 @@ background = 0
 for i in range(30):
     ret,background = cap.read()
     
+while(cap.isOpened()):
+    ret,img = cap.read()
+    if not ret:
+        break
+    hsv=cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
+    
