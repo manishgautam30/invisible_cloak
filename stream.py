2fb75dfb6148179aac72a69b5a56b6a263799518
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-07-24 16:32:47 +0530

    background is the image when not in front of cam

--- stream.py
+++ stream.py
@@ -32,3 +32,9 @@ while(cap.isOpened()):
     mask1 = cv2.morphologyEx(mask1 ,cv2.MORPH_DILATE,np.ones((3,3),np.uint8),iterations=1)
 
     mask2 = cv2.bitwise_not(mask1)
+
+    res1 = cv2.bitwise_and(background, background, mask = mask1) 
+    res2 = cv2.bitwise_and(img, img, mask = mask2) 
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0) 
+
+    
