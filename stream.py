c12853b770be4594b3fa4e659a488c662c0aab33
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-07-24 16:13:03 +0530

    reduce noise that may occur in the img capturing

--- stream.py
+++ stream.py
@@ -16,4 +16,17 @@ while(cap.isOpened()):
         break
     hsv=cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
     
-    lower_red = np.array(0, 120 , 70)
+    lower_red = np.array([0, 120 , 70])
+    upper_red = np.array([0, 255 , 255])
+
+    mask1=cv2.inRange(hsv.lower_red,upper_red)
+    
+    lower_red = np.array([170, 120 , 70])
+    upper_red = np.array([180, 255 , 255])
+
+    mask2=cv2.inRange(hsv.lower_red,upper_red)
+
+    mask1 = mask1 + mask2
+
+    mask1 = cv2.morphologyEx(mask1 ,cv2.MORPH_OPEN,np.ones((3,3),np.uint8),iterations=2)
+    mask1 = cv2.morphologyEx(mask1 ,cv2.MORPH_DILATE,np.ones((3,3),np.uint8),iterations=1)
